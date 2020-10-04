686ffc26df4f20b1c7925375373b1731cd17e410
govindpg <72184197+govindpg@users.noreply.github.com>
2020-10-04 17:43:09 +0530

    Update AddInteger.java

--- Java Programs/AddInteger.java
+++ Java Programs/AddInteger.java
@@ -4,9 +4,10 @@ public class AddTwoIntegers {
         
         int first = 10;
         int second = 20;
+        int  third = 30;
 
-        System.out.println("Enter two numbers: " + first + " " + second);
-        int sum = first + second;
+        System.out.println("Enter two numbers: " + first + " " + second+" "+third);
+        int sum = first + second +third;
 
         System.out.println("The sum is: " + sum);
     }
