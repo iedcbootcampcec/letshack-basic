07a016574bcbcf2f8a01b8359d099443ff6006b9
Brad <bharrod321@gmail.com>
2020-10-04 10:42:15 -0500

    Create Case.java

---
+++ Java Programs/Case.java
@@ -0,0 +1,23 @@
+public class caseStatements {
+
+  public static void main(String[] args) {
+    
+    int meal = 4;
+    switch (meal) {
+    case 1:
+    System.out.println("Breakfast");
+    break;
+   case 2:
+    System.out.println("Lunch");
+    break;
+    case 3:
+    System.out.println("Dinner");
+    break;
+   default:
+    System.out.println("How many times do you eat?");
+}
+
+
+    }  
+  }
+}
