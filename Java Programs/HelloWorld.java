49b0d0085f5beb2498aa366ff06f7c442603bfee
govindpg <72184197+govindpg@users.noreply.github.com>
2020-10-04 17:19:17 +0530

    Update HelloWorld.java

--- Java Programs/HelloWorld.java
+++ Java Programs/HelloWorld.java
@@ -1,6 +1,10 @@
 class HelloWorld {
     public static void main (String [] args) 
     {
-        System.out.print(" Hello World!");
+        System.out.println(" Hello World!");
+        int i=4;
+        int j=6;
+        int sum = i+j;
+        System.out.println("sum of i and j is;"+sum);
     }
 }
