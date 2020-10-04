0abe363656d718ec1549a5addf8ddca2921fb3e9
MarioEros <erosrodespi@gmail.com>
2020-10-04 11:23:39 +0200

    Uploaded C# class for casting int to time HH:MM:SS

---
+++ C#/Human_Readable_Time.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Human_Readable_Time
+{
+    public static string GetReadableTime(int seconds)
+    {
+        return "" +
+            ((((int)seconds / 3600) < 10 ? "0" + ((int)seconds / 3600) : "" + ((int)seconds / 3600)) + ":" +
+            (((int)(seconds % 3600) / 60) < 10 ? "0" + ((int)(seconds % 3600) / 60) : "" + ((int)(seconds % 3600) / 60)) + ":" +
+            (((int)seconds % 3600 % 60) < 10 ? "0" + ((int)seconds % 3600 % 60) : "" + ((int)seconds % 3600 % 60)));
+    }
+}
