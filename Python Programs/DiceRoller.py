3ecb9197a04751b09232678a250a9e50444c1bb5
Gautham-007 <72331838+Gautham-007@users.noreply.github.com>
2020-10-04 11:58:24 +0530

    Update DiceRoller.py

--- Python Programs/DiceRoller.py
+++ Python Programs/DiceRoller.py
@@ -10,4 +10,4 @@ while roll_again == "yes" or roll_again == "y":
     print random.randint(min, max)
     print random.randint(min, max)
 
-    roll_again = raw_input("Roll the dices again?")
+    roll_again = raw_input("Roll the dices again?")ghg
