58b04ab18dce9675e7817fa51dc6ff8f153f2c39
Devika-H1 <72244373+Devika-H1@users.noreply.github.com>
2020-10-04 12:01:24 +0530

    Update HelloWorld.java

--- Java Programs/HelloWorld.java
+++ Java Programs/HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main (String [] args) 
+    {
+        System.out.print(" Hello World!");
     }
 }
