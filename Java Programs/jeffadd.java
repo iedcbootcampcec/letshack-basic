7402f9de04f0c52631b6c1affb7a423e146ae267
jeff-aaron-johns <72004339+jeff-aaron-johns@users.noreply.github.com>
2020-10-11 21:14:16 +0530

    Create jeffadd.java

---
+++ Java Programs/jeffadd.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 25, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum is: "+sum);
+   }
+}
