8916fdc44be6e895020ba94204c7a6403c70c7ff
anjima1008 <62937593+anjima1008@users.noreply.github.com>
2020-10-16 20:45:27 +0530

    Create anjima.py

---
+++ Python Programs/anjima.py
@@ -0,0 +1,10 @@
+#program tofind the reverse of string
+def string_reverse(str1):
+
+    rstr1 = ''
+    index = len(str1)
+    while index > 0:
+        rstr1 += str1[ index - 1 ]
+        index = index - 1
+    return rstr1
+print(string_reverse('1234abcd'))
