5ae632faaf794b469502bf95aa41faaa78f781ff
anjima1008 <62937593+anjima1008@users.noreply.github.com>
2020-10-16 20:11:49 +0530

    Create Anjima K Vijayan.c

---
+++ C Programs/Anjima K Vijayan.c
@@ -0,0 +1,22 @@
+//Program written to display the multiplication table of a no 
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int i;
+  int n;
+  printf("Enter the number:\n");
+  scanf("%d",&i);
+  i*n;
+ 
+  for(n=1;n<=10;n++)
+  {
+   
+    printf("%d*%d=%d\n" ,i,n,i*n);
+  }
+ return 0;
+}
+ 
