ae20ee69493a8bb495c025ee342ea878db25e1b2
aswindevmp <72143783+aswindevmp@users.noreply.github.com>
2021-10-01 14:59:08 +0530

    Update Sum of digits.java

--- Sum of digits.java
+++ Sum of digits.java
@@ -3,15 +3,15 @@ public class Digit_Sum
 {
     public static void main(String args[])
     {
-        int m, n, sum = 0;
+        int x, n, sum = 0;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the number:");
-        m = s.nextInt();
-        while(m > 0)
+        x = s.nextInt();
+        while(x > 0)
         {
-            n = m % 10;
+            n = x % 10;
             sum = sum + n;
-            m = m / 10;
+            x = x / 10;
         }
         System.out.println("Sum of Digits:"+sum);
     }
