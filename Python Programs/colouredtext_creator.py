539d477c92141e55015e4d01b6f207e29bffbe9b
Govind P G <72184197+govindpg@users.noreply.github.com>
2021-10-03 19:56:31 +0530

    Create colouredtext_creator.py

---
+++ Python Programs/colouredtext_creator.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is hope :) "+ Fore.YELLOW+ Back.BLUE+"hope makes life beauty")
+print(Back.CYAN+"Hi My name is hope :) ")
+print(Fore.RED + Back.GREEN+ "Hi My name is hope :) ")
