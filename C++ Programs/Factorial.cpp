7d29cc0b1c3a7b992ef34be1c356a28d0bc6824c
Amarjeet Sinha <54766242+amarjeetsinha@users.noreply.github.com>
2021-10-05 00:05:29 +0530

    Update Factorial.cpp

--- C++ Programs/Factorial.cpp
+++ C++ Programs/Factorial.cpp
@@ -2,11 +2,33 @@
 #include<conio.h>
 using namespace std;
 
+/*
+
+//For n>16 will not work with this function.//
+
 int fact(int n)
 {
    if(n==0)
-        return 0;
-   return n*factorial (n-1);
+        return 1;
+   return n*fact(n-1);
+}
+*/
+
+//Using recursion in Factorial may cause stack overflow problem if the recursive call is large in number, so it needs change..
+
+//----------------------------------------------------------------// CHANGED CODE//----------------------------------------------------------------//
+
+unsigned long long int fact(unsigned long long int n)
+{
+    unsigned long long int fact = 1;
+    unsigned long long int i=2;
+    while (i<=n)
+    {
+        fact = fact * i;
+        i++;
+    }
+    return fact;
+//THIS PROGRAM CAN GIVE FACTORIAL OF UPTO 65 NUMBER. 
 }
 
 int main()
