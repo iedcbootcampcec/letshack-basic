a9ffcdcd7a0bd423fbdfb87a28751ded92c36ab1
Nivinvigi <73237270+Nivinvigi@users.noreply.github.com>
2021-10-04 05:57:39 +0530

    Add files via upload

---
+++ kmToMiles.py
@@ -0,0 +1,9 @@
+#taking input from user
+kilometres=float(input("Enter the distance in kilometres:"))
+
+#conversion factor
+con_fact=0.621371
+
+miles=con_fact*kilometres
+
+print("%0.2f kilometres is equal to %0.2f miles"%(kilometres,miles))
