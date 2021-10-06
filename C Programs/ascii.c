de99b8db7e388ac025df7b74bc7fe6c4f69782b5
Igoraza <86516457+Igoraza@users.noreply.github.com>
2021-10-06 15:46:35 +0530

    Create ascii.c

---
+++ C Programs/ascii.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ int main() {
+   char c;
+   printf("https://github.com/Igoraza");              
+   printf("Enter a character: ");
+   scan("%c" , &c);
+   print("ASSII value =%d, c);
+         return 0;
