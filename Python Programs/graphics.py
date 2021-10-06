a6739a6b7e58042a62ac4a5ac24ba2dc14d7aad9
Athul Mathew Varughese <68552836+Athulmathew01@users.noreply.github.com>
2021-10-07 00:18:28 +0530

    turtle graphics in python
    just a sample program to draw 'H' using turtle module in python

---
+++ Python Programs/graphics.py
@@ -0,0 +1,29 @@
+import turtle
+
+
+t=turtle.Turtle()
+
+wd =turtle.Screen()
+
+t.color("pink")
+
+t.width(3)
+
+  
+
+
+
+t.right(90)
+t.forward(150)
+t.back(75)
+
+t.right(-112)
+t.forward(100)
+
+t.right(113)
+t.back(75)
+t.forward(150)
+
+
+
+turtle.exitonclick()
