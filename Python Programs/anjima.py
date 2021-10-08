552c5c6928b48736f5de9ac8e12dfcbc383f5003
shyamkrishnan1999 <shyamkrishnan1999@gmail.com>
2021-10-08 10:24:13 +0530

    Some minor changes

--- Python Programs/anjima.py
+++ Python Programs/anjima.py
@@ -1,10 +1,6 @@
 #program tofind the reverse of string
 def string_reverse(str1):
+    return str1[::-1]   #pythonic way of finding reverse of a string using list slicing
 
-    rstr1 = ''
-    index = len(str1)
-    while index > 0:
-        rstr1 += str1[ index - 1 ]
-        index = index - 1
-    return rstr1
-print(string_reverse('1234abcd'))
+mystring=input("Enter a String:")
+print("The Reverse is:"+string_reverse(mystring))
