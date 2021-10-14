3485422b473b5e2bea71e199687a9db2d35bf547
Harshithardd <79263383+Harshithardd@users.noreply.github.com>
2021-10-14 19:30:07 +0530

    added print_calendar using python

---
+++ Python Programs/print_calendar.py
@@ -0,0 +1,8 @@
+#This program helps you to print the calendar using python code
+
+import calendar
+year=2021
+month=4
+year=int(input("enter a year:"))
+month=int(input("enter a month:"))
+print(calendar.month(year,month))
