bb2a1ec0b72ca9f7065a2f6d022b592ca8f4a5bc
Navya Susan Thomas <62889118+nst-2001@users.noreply.github.com>
2021-10-18 11:23:17 +0530

    Create nststring2double.java

---
+++ Java Programs/nststring2double.java
@@ -0,0 +1,19 @@
+
+
+public class STD {
+
+	
+	public static void main(String args[])
+	{
+
+		
+		String str = "2033.12244";
+
+		
+	
+		double str1 = Double.parseDouble(str);
+
+	
+		System.out.println(str1);
+	}
+}
