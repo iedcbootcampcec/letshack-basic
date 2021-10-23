6db2451972af159699fe03f53657826e0d08575c
KevintheChuker <92706417+KevintheChuker@users.noreply.github.com>
2021-10-23 15:58:29 -0700

    Update Calculator.py

--- Python Programs/Calculator.py
+++ Python Programs/Calculator.py
@@ -1,30 +1,10 @@
-operation = input("Would you like to 1: add, 2: subtract, 3: multiply, 4: divide or 5:find total of a power(enter a number): ")
-
-if operation != 5:
-  
-  a = float(input("What's your first number? "))
-  b = float(input("What's your second number? "))
-  total = 0.0
-
-  if operation == 1:
-    total = a + b
-
-  if operation == 2:
-    total = a - b
-
-  if operation == 3:
-    total = a * b
-
-  else: 
-    total = a // b
-
-  print ("Your answer is " + (total))
+operation = int(input("Would you like to 1: add, 2: subtract, 3: multiply, 4: divide or 5:find total of a power(enter a number): "))
+total = 0
 
 if operation == 5:
 
   base = input("What is your base number(no fractions, only decimals): ") 
   exp = input("What is your exponent(whole numbers only): ")
-  total = 0
 
   base = float(base)
   exp = int(exp)
@@ -37,3 +17,22 @@ if operation == 5:
     exp = exp - 1
 
   print(total)
+
+else:
+  
+  num1 = int(input("What's your first number? "))
+  num2 = int(input("What's your second number? "))
+  
+  if operation == 1:
+    total = num1 + num2
+
+  elif operation == 2:
+    total = num1 - num2
+
+  elif operation == 3:
+    total = num1 * num2
+
+  elif operation == 4: 
+    total = num1 // num2
+
+print ("Your answer is " + str(total))
