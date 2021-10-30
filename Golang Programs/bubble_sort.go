921b040b547dcdfb20a8e4a06a4a3462805e6dcc
Suradid Chao <chill_shyld@hotmail.com>
2021-10-30 22:22:55 +0700

    add bubble sort

---
+++ Golang Programs/bubble_sort.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) []int {
+	for range arr {
+		for j, _ := range arr {
+			if j >= len(arr) - 1 {
+				break
+			}
+			if arr[j] > arr[j+1] {
+				temp := arr[j+1]
+				arr[j+1] = arr[j]
+				arr[j] = temp
+			}
+		}
+	}
+
+	return arr
+}
+
+func main() {
+	usArr := []int{5, 2, 6, 3, 1, 4}
+	sArr := bubbleSort(usArr)
+	fmt.Println(sArr)
+}
